d07e2a6974c1f5c18877c7c0b83db315d98e66d0
ameydeshpande18 <ameydeshpande.18@gmail.com>
2019-10-18 16:09:12 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import createReport from "docx-templates";
+import { saveAs } from "file-saver";
 
 @Component({
   selector: "my-app",
@@ -7,13 +8,18 @@ import createReport from "docx-templates";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  download() {
+  async download() {
     console.log(createReport);
 
-    createReport({
+    const report = await createReport({
       template: "../assets/test.docx",
       output: "output.docx",
-      data: {'test':'this is test data'}
+      data: { test: "this is test data" }
     });
+    const blob = new Blob([report], {
+      type:
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    });
+    saveAs(blob, "abc.docx");
   }
 }

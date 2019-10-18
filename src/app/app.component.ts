38c879a8baf91865ec6064069c4ee84534dabd8b
ameydeshpande18 <ameydeshpande.18@gmail.com>
2019-10-18 17:32:59 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,41 @@ import { saveAs } from "file-saver";
 })
 export class AppComponent {
   download() {
-
-    // const report = await createReport({
-    //   template: "../assets/test.docx",
-    //   output: "output.docx",
-    //   data: {}
-    // });
-    // console.log(report);
-
-    const blob = new Blob(['testing'], {
-      type:
-        "text/plain;charset=utf-8"
+    const report = createReport({
+      template: "assets/test.docx",
+      output: "output.docx",
+      data: {}
     });
+    console.log(report);
+
+    // const blob = new Blob(report, {
+    //   type: "text/plain;charset=utf-8"
+    // });
     // saveAs(blob, "abc.docx");
+
+    this.saveDataToFile(
+      report,
+      "report.docx",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    );
   }
+
+  saveDataToFile = (data, fileName, mimeType) => {
+    const blob = new Blob([data], { type: mimeType });
+    const url = window.URL.createObjectURL(blob);
+    this.downloadURL(url, fileName);
+    setTimeout(() => {
+      window.URL.revokeObjectURL(url);
+    }, 1000);
+  };
+
+  downloadURL = (data, fileName) => {
+    const a = document.createElement("a");
+    a.href = data;
+    a.download = fileName;
+    document.body.appendChild(a);
+     a.style.display = 'none';
+    a.click();
+    a.remove();
+  };
 }
